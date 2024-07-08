799474797e0b66f7f90825bc15e45e3904dc9369
Luis albarran <luisalbarran21rl04091216@gmail.com>
2024-07-08 17:05:55 -0500

    secondchange

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,14 @@ export default function Home() {
   
   return (
     <main >
-      <img src="/banner.png" alt="banner" className=" w-full h-[400px] md:h-[500px] object-fill" />
-      <div className=" h-screen py-40" style={{backgroundImage: 'url("/patron1.png")', backgroundSize: "cover"}}>
-      <p className="text-center font-bold text-6xl text-[#62C7F1]">Ultimos eventos</p>
-      <div>
-        <img src="" alt="" />
+      <img src="/banner.png" alt="banner" className=" w-full h-[400px] md:min-h-[500px] object-fill" />
+      <div className=" min-h-screen py-40 " style={{backgroundImage: 'url("/patron1.png")', backgroundSize: "cover"}}>
+      <p className="text-center font-bold text-6xl text-[#62C7F1] mb-12">Ultimos eventos</p>
+      <div className=" flex flex-wrap max-w-[1200px] gap-3 justify-center items-center mx-auto">
+        <img src="/evento1.jpeg" alt="" className="w-[300px] h-[300px] object-cover rounded-md" />
+        <img src="/evento2.jpeg" alt="" className="w-[300px] h-[300px] object-cover rounded-md" />
+        <img src="/evento3.jpeg" alt="" className="w-[300px] h-[300px] object-cover rounded-md" />
+        <img src="/evento4.jpeg" alt="" className="w-[300px] h-[300px] object-cover rounded-md" />
       </div>
       </div>
 
