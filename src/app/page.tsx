61e4879691e832ff64c715ec4c98bec59fdb75a8
Luis albarran <luisalbarran21rl04091216@gmail.com>
2024-07-08 16:59:11 -0500

    secondchange

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,13 @@ export default function Home() {
   return (
     <main >
       <img src="/banner.png" alt="banner" className=" w-full h-[400px] md:h-[500px] object-fill" />
+      <div className=" h-screen py-40" style={{backgroundImage: 'url("/patron1.png")', backgroundSize: "cover"}}>
+      <p className="text-center font-bold text-6xl text-[#62C7F1]">Ultimos eventos</p>
+      <div>
+        <img src="" alt="" />
+      </div>
+      </div>
+
     </main>
   );
 }
