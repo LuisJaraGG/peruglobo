b3788c6e02882015a2d9d821f304913112d1050f
Luis albarran <luisalbarran21rl04091216@gmail.com>
2024-08-08 19:05:34 -0500

    admin fixed

--- src/app/(pages)/(admin)/admin/dashboard/page.tsx
+++ src/app/(pages)/(admin)/admin/dashboard/page.tsx
@@ -8,7 +8,7 @@ import TableProducts from "../_components/tableProducts";
 export default function Dashboard() {
     return (
         <>
-        <h1 className="text-3xl text-white font-bold">Lista de productos</h1>
+        <h1 className="text-3xl text-white font-bold mb-5 text-center">Lista de productos</h1>
         <main className=" ">
             <TableProducts></TableProducts>
         </main>
--- src/app/(pages)/(admin)/admin/layout.tsx
+++ src/app/(pages)/(admin)/admin/layout.tsx
@@ -3,17 +3,20 @@ import { Inter } from "next/font/google";
 import Sidebar from "./_components/sidebar";
 const inter = Inter({ subsets: ["latin"] });
 import { Toaster } from 'react-hot-toast';
+
 export default function AdminLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <>
-        <main suppressHydrationWarning className="bg-slate-800 min-h-screen">
-        <Sidebar children={children} />
-        </main>
-      </>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <main suppressHydrationWarning className="bg-slate-800 min-h-screen">
+        <Sidebar />
+        <div className="md:pl-[265px] pl-5 pr-5 pt-5 relative">
+          {children}
+        </div>
+      </main>
+    </>
+  );
+}
